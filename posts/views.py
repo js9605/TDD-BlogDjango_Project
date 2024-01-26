5276c3b053d2923c539bb1b5b30fa40008e65ce0
Jacek <jacekszwarc1@gmail.com>
2024-01-26 22:35:11 +0100

    Building a list View

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, "posts/index.html")
+    posts = Post.objects.all()
+    return render(request, "posts/index.html", {"posts": posts})
